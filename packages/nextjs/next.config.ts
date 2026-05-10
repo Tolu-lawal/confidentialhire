966ee2a0ab0feb8b29529b9d08eb044c018133dc
Tolu-lawal <lawaltolu00@gmail.com>
2026-05-10 16:25:51 +0100

    add turbopack config

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  turbopack: {},
   devIndicators: false,
   onDemandEntries: {
     maxInactiveAge: 25 * 1000,
