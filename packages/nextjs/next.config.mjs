6d52ff98ce2f3c9c035be728af7378d84addbc1d
Tolu-lawal <lawaltolu00@gmail.com>
2026-05-10 18:41:32 +0100

    add webpack externals

--- packages/nextjs/next.config.mjs
+++ packages/nextjs/next.config.mjs
@@ -1,5 +1,9 @@
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.externals.push('pino', 'pino-pretty', 'lokijs', 'encoding', '@react-native-async-storage/async-storage');
+    return config;
+  },
 };
 
 export default nextConfig;
