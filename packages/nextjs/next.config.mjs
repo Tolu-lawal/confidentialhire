d661fc9d76a28b9dc9d656099c82fc81fe473691
Tolu-lawal <lawaltolu00@gmail.com>
2026-05-10 18:38:36 +0100

    minimal mjs config

--- packages/nextjs/next.config.mjs
+++ packages/nextjs/next.config.mjs
@@ -1,26 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   reactStrictMode: true,
-  devIndicators: false,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
-        ],
-      },
-    ];
-  },
-  webpack: config => {
-    config.externals.push("pino", "pino-pretty", "lokijs", "encoding", "@react-native-async-storage/async-storage");
-    return config;
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
