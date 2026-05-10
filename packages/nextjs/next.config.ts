944b2707f2b97e85e3ee25aebb48023d9665d63e
Tolu-lawal <lawaltolu00@gmail.com>
2026-05-10 17:47:52 +0100

    simplify next config

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -1,16 +1,13 @@
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  turbopack: {},
   devIndicators: false,
-  onDemandEntries: {
-    maxInactiveAge: 25 * 1000,
-  },
   typescript: {
-    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    ignoreBuildErrors: true,
   },
   eslint: {
-    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    ignoreDuringBuilds: true,
   },
   async headers() {
     return [
@@ -23,23 +20,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-webpack: (config, { isServer }) => {
-  if (!isServer) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      "@react-native-async-storage/async-storage": false,
-    };
-  }
-  config.externals.push("pino-pretty", "lokijs", "encoding", "@react-native-async-storage/async-storage");
-  return config;
-},
 };
-
-const isIpfs = process.env.NEXT_PUBLIC_IPFS_BUILD === "true";
-if (isIpfs) {
-  nextConfig.output = "export";
-  nextConfig.trailingSlash = true;
-  nextConfig.images = { unoptimized: true };
-}
 
 module.exports = nextConfig;
